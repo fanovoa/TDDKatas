97335b562baa621cc6bd0a94ba5bab4a464c3c0c
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 18:53:13 -0500

    🔴Si_Ingreso_La_Palabra_oro_debe_retornar_true

--- TDDKatas.Test/Palindromos/PalindromosTest.cs
+++ TDDKatas.Test/Palindromos/PalindromosTest.cs
@@ -1,6 +1,19 @@
-﻿namespace TDDKatas.Palindromos;
+﻿using FluentAssertions;
+
+namespace TDDKatas.Palindromos;
 
 public class PalindromosTest
 {
-    
+    [Fact]
+    public void Si_Ingreso_La_Palabra_oro_debe_retornar_true()
+    {
+        var palabra = "oro";
+        var esPalindromo = ValidarSiEsLaPalabraEsPalindromo(palabra);
+        esPalindromo.Should().Be(true);
+    }
+
+    private object ValidarSiEsLaPalabraEsPalindromo(string palabra)
+    {
+        throw new NotImplementedException();
+    }
 }
