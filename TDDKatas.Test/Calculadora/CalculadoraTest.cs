1e2deb8b8c6952c8642258150f0f9ca4604c8b8d
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 23:45:15 -0500

    🟢Si_Cadena_Contiene_Simbolo_Admiracion_DEBE_Retornar_IngreseNumeroValido

--- TDDKatas.Test/Calculadora/CalculadoraTest.cs
+++ TDDKatas.Test/Calculadora/CalculadoraTest.cs
@@ -49,6 +49,7 @@ public class CalculadoraTest
         
         numero = ConvertirAMinusculas(numero);
         if (EsVacioNulo(numero) || ContieneLetras(numero)) return MENSAJE_ERROR;
+        if(numero.Contains("!")) return MENSAJE_ERROR;
 
         return "número válido";
     }
