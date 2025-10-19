bb1de339cbe22e6ec04e1e405b30402ebdc394ba
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 19:14:54 -0500

    🔴Si_ingreso_una_palabra_conSignoAdmiracion_debe_retornar_true

--- TDDKatas.Test/Palindromos/PalindromosTest.cs
+++ TDDKatas.Test/Palindromos/PalindromosTest.cs
@@ -14,6 +14,13 @@ public class PalindromosTest
         var esPalindromo = ValidarSiEsLaPalabraEsPalindromo(palabra);
         esPalindromo.Should().Be(true);
     }
+
+    [Fact]
+    public void Si_ingreso_una_palabra_conSignoAdmiracion_debe_retornar_true()
+    {
+        var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("ana!");
+        esPalindromo.Should().Be(true);
+    }
     
     private bool ValidarSiEsLaPalabraEsPalindromo(string palabra)
     {
