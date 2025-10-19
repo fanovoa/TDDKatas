1d44c26a40207b5ce412dfb290108124d8640323
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 23:48:37 -0500

    🟢Si_Cadena_Contiene_Simbolo_Correo_DEBE_Retornar_IngreseNumeroValido

--- TDDKatas.Test/Calculadora/CalculadoraTest.cs
+++ TDDKatas.Test/Calculadora/CalculadoraTest.cs
@@ -63,7 +63,7 @@ public class CalculadoraTest
         
         numero = ConvertirAMinusculas(numero);
         if (EsVacioNulo(numero) || ContieneLetras(numero)) return MENSAJE_ERROR;
-        if(numero.Contains("!") || numero.Contains("??")) return MENSAJE_ERROR;
+        if(numero.Contains("!") || numero.Contains("??") || numero.Contains("@")) return MENSAJE_ERROR;
 
         return "número válido";
     }
