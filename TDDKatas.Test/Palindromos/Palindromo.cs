116e5e55d1830ff4c9a2722106a106dbdcd5b991
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 21:26:33 -0500

    🟢Si_ingresa_null_debe_retornar_false

--- TDDKatas.Test/Palindromos/Palindromo.cs
+++ TDDKatas.Test/Palindromos/Palindromo.cs
@@ -6,6 +6,7 @@ public class Palindromo(string texto)
 
     public bool ValidarSiEsLaPalabraEsPalindromo()
     {
+        if (string.IsNullOrEmpty(Texto)) return false;
         ConvertirAMinusculas();
         if (ContieneSimbolos()) ReemplazaSimbolos();
         if (ContieneAcentos()) ReemplazaAcentos();
