f19fed57386d0df2a1289e56201faea302575e40
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 23:06:56 -0500

    🔴Si_Ingreso_Vacio_DEBE_Retornar_IngreseNumeroValido

---
+++ TDDKatas.Test/Calculadora/CalculadoraTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+
+namespace TDDKatas.Calculadora;
+
+public class CalculadoraTest
+{
+    [Fact]
+    public void Si_Ingreso_Vacio_DEBE_Retornar_IngreseNumeroValido()
+    {
+        var numero = "";
+        var resultado = ValidarCadena();
+        resultado.Should().Be("Ingrese un número válido.");
+    }
+
+    private object ValidarCadena()
+    {
+        throw new NotImplementedException();
+    }
+}
