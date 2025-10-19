41be53c33c23c870866c7766c24268b34a9bdf50
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 19:22:49 -0500

    🔴Si_ingreso_una_palabra_conAsterisco_debe_retornar_true

--- TDDKatas.Test/Palindromos/PalindromosTest.cs
+++ TDDKatas.Test/Palindromos/PalindromosTest.cs
@@ -28,6 +28,13 @@ public class PalindromosTest
         var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("an,a");
         esPalindromo.Should().Be(true);
     }
+
+    [Fact]
+    public void Si_ingreso_una_palabra_conAsterisco_debe_retornar_true()
+    {
+        var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("*ana");
+        esPalindromo.Should().Be(true);
+    }
     
     private bool ValidarSiEsLaPalabraEsPalindromo(string palabra)
     {
