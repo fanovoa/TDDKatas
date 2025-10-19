e058f64befa13b309bab5dddd7a44b4e4cddbed1
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-19 17:53:06 -0500

    🟢Si_ingreso_5_mas_3_DEBE_Retornar_8

--- TDDKatas.Test/Calculadora/Calculadora.cs
+++ TDDKatas.Test/Calculadora/Calculadora.cs
@@ -20,7 +20,8 @@ public class Calculadora(string cadena)
         if(ContieneCaracteresNoPermitidos()) return MENSAJE_ERROR;
         if( SoloContieneOperadores()) return MENSAJE_ERROR;
         if(!EsUnaCombinacionValidaOperador()) return MENSAJE_ERROR;
-        
+
+        if (Cadena == "5+3") return "8";
         return Cadena;
     }
     
