25688af1d5962b16733ccf912d62c132e4066145
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-18 19:42:34 -0500

    🔵Si_ingreso_una_palabra_palindroma_que_contenga_algunSimbolo_debe_retornar_true

--- TDDKatas.Test/Palindromos/PalindromosTest.cs
+++ TDDKatas.Test/Palindromos/PalindromosTest.cs
@@ -14,33 +14,38 @@ public class PalindromosTest
         var esPalindromo = ValidarSiEsLaPalabraEsPalindromo(palabra);
         esPalindromo.Should().Be(true);
     }
-
-    [Fact]
-    public void Si_ingreso_una_palabra_conSignoAdmiracion_debe_retornar_true()
+    
+    [Theory]
+    [InlineData("an,a")]
+    [InlineData("ana!")]
+    [InlineData("*ana")]
+    [InlineData("*a!?na")]
+    public void Si_ingreso_una_palabra_palindroma_que_contenga_algunSimbolo_debe_retornar_true(string palabra)
     {
-        var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("ana!");
-        esPalindromo.Should().Be(true);
-    }
-
-    [Fact]
-    public void Si_ingreso_una_palabra_conComa_debe_retornar_true()
-    {
-        var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("an,a");
-        esPalindromo.Should().Be(true);
-    }
-
-    [Fact]
-    public void Si_ingreso_una_palabra_conAsterisco_debe_retornar_true()
-    {
-        var esPalindromo =ValidarSiEsLaPalabraEsPalindromo("*ana");
+       var esPalindromo =ValidarSiEsLaPalabraEsPalindromo(palabra);
         esPalindromo.Should().Be(true);
     }
     
     private bool ValidarSiEsLaPalabraEsPalindromo(string palabra)
     {
-        if (palabra.Contains("!") || palabra.Contains(",") || palabra.Contains("*"))
-            palabra = palabra.Replace("!", "").Replace(",", "").Replace("*","");
+        if (ContieneSimbolos(palabra))
+            palabra = RemplazaSimbolos(palabra);
+        
         var palabraAlReves = palabra.ToCharArray().Reverse();
         return palabra == new string(palabraAlReves.ToArray());
+    }
+
+    private static string RemplazaSimbolos(string palabra)
+    {
+        var quitarSimbolos = palabra
+            .Where(char.IsLetterOrDigit)
+            .ToArray();
+        return  new string(quitarSimbolos);
+    }
+
+    private static bool ContieneSimbolos(string palabra)
+    {
+        return palabra.Any(caracter => char.IsLetterOrDigit(caracter));
+       
     }
 }
