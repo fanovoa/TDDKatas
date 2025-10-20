dfc58cb6c1ba3a8c9e09a046b10f9293cfd6902a
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-20 10:25:19 -0500

    🟢Si_ingreso_vacio_DEBE_retornar_la_posicion_inicial_0_0_N

--- TDDKatas.Test/MarsRover/MarsRoverTests.cs
+++ TDDKatas.Test/MarsRover/MarsRoverTests.cs
@@ -1,6 +1,21 @@
-﻿namespace TDDKatas.MarsRover;
+﻿using FluentAssertions;
+
+namespace TDDKatas.MarsRover;
 
 public class MarsRoverTests
 {
-    
+    [Fact]
+    public void Si_ingreso_vacio_DEBE_retornar_la_posicion_inicial_0_0_N()
+    {
+        var comando = "";
+        var posicionFinal = CalcularPosicion(comando);
+        posicionFinal.Should().Be("0:0:N");
+    }
+
+    private object CalcularPosicion(string comando)
+    {
+        if( comando =="")
+            return "0:0:N";
+        return "";
+    }
 }
