5a16388f830c107e22e6369d8a1476a88faa2d65
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-20 12:38:16 -0500

    🔵 Refactor. (se crea archivo Clase MarsRover)

---
+++ TDDKatas.Test/MarsRover/MarsRover.cs
@@ -0,0 +1,43 @@
+﻿namespace TDDKatas.MarsRover;
+
+public class MarsRover
+{
+    public string CalcularPosicion(string comandos)
+    {
+        int posicion_X = 0;
+        int posicion_Y = 0;
+        int orientacion_inicial = 0;
+        char[] orientacion = ['N','E','S','W'];
+        
+        foreach (var comando in comandos.ToCharArray())
+        {
+            if (comando == 'M')
+            {
+                switch (orientacion[orientacion_inicial])
+                {
+                    case 'E':
+                        posicion_X++;
+                        break;
+                    case 'W':
+                        posicion_X--;
+                        break;
+                    case 'S':
+                        posicion_Y--;
+                        break;
+                    default:
+                        posicion_Y++;
+                        break;
+                }
+            }
+            if (comando == 'R') orientacion_inicial++;
+            if (comando == 'L') orientacion_inicial--;
+            
+            if(orientacion_inicial >3 ) orientacion_inicial = 0;
+            if (orientacion_inicial <0 ) orientacion_inicial = 3;
+            
+        }
+        
+        return string.Concat(posicion_X,":", posicion_Y,":",orientacion[orientacion_inicial]);
+        
+    }
+}
--- TDDKatas.Test/MarsRover/MarsRoverTests.cs
+++ TDDKatas.Test/MarsRover/MarsRoverTests.cs
@@ -4,11 +4,15 @@ namespace TDDKatas.MarsRover;
 
 public class MarsRoverTests
 {
+
     [Fact]
     public void Si_ingreso_vacio_DEBE_retornar_la_posicion_inicial_0_0_N()
     {
-        var comando = "";
-        var posicionFinal = CalcularPosicion(comando);
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion("");
+        //assert
         posicionFinal.Should().Be("0:0:N");
     }
     
@@ -19,7 +23,11 @@ public class MarsRoverTests
     [InlineData("MMMM","0:4:N")]
     public void Si_ingreso_solo_movimientos_DEBE_avanzar_las_mismas_casillas_del_movimiento_en_Y(string comando, string esperado)
     {
-        var posicionFinal = CalcularPosicion(comando);
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion(comando);
+        //assert
         posicionFinal.Should().Be(esperado);
     }
     
@@ -30,72 +38,55 @@ public class MarsRoverTests
     [InlineData("MRRRR","0:1:N")]
     public void Si_solo_avanza_una_posicion_y_gira_n_veces_a_la_derecha_DEBE_mantener_el_primer_movimento_y_solo_cambiar_su_orientacion(string comando, string posicionFinalEsperada)
     {
-        var posicionFinal = CalcularPosicion(comando);
+        
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion(comando);
+        //assert
         posicionFinal.Should().Be(posicionFinalEsperada);
     }
     [Fact]
     public void Si_ingreso_un_movimiento_y_un_giro_a_la_izquierda_DEBE_avanzar_un_movimieto_y_orientarse_al_oeste()
     {
-        var posicionFinal = CalcularPosicion("ML");
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion("ML");
+        //assert
         posicionFinal.Should().Be("0:1:W");
     }
 
     [Fact]
     public void Si_ingreso_un_movimiento_giro_a_la_derecha_y_otro_movimiento_DEBE_orientarse_al_este_y_avanzar_en_x_como_en_y()
     {
-        var posicionFinal = CalcularPosicion("MRM");
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion("MRM");
+        //assert
         posicionFinal.Should().Be("1:1:E");
     }
 
     [Fact]
     public void Si_ingreso_un_movimiento_giro_a_la_izquierda_y_otro_movimiento_DEBE_orientarse_al_oeste_y_disminuir_en_x_y_avanzar_en_y()
     {
-        var posicionFinal = CalcularPosicion("MLM");
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion("MLM");
+        //assert
         posicionFinal.Should().Be("-1:1:W");
     }
 
     [Fact]
     public void Si_se_oriente_a_sur_y_avanza_n_posiciones_DEBE_disminuir_en_y_mantenerse_en_x()
     {
-        var posicionFinal = CalcularPosicion("LLM");
+        //arrange
+        var marsRover = new MarsRover();
+        //act
+        var posicionFinal = marsRover.CalcularPosicion("LLM");
+        //assert
         posicionFinal.Should().Be("0:-1:S");
-    }
-    private string CalcularPosicion(string comandos)
-    {
-        int posicion_X = 0;
-        int posicion_Y = 0;
-        int orientacion_inicial = 0;
-        char[] orientacion = ['N','E','S','W'];
-        
-        foreach (var comando in comandos.ToCharArray())
-        {
-            if (comando == 'M')
-            {
-                switch (orientacion[orientacion_inicial])
-                {
-                    case 'E':
-                        posicion_X++;
-                        break;
-                    case 'W':
-                        posicion_X--;
-                        break;
-                    case 'S':
-                        posicion_Y--;
-                        break;
-                    default:
-                        posicion_Y++;
-                        break;
-                }
-            }
-            if (comando == 'R') orientacion_inicial++;
-            if (comando == 'L') orientacion_inicial--;
-            
-            if(orientacion_inicial >3 ) orientacion_inicial = 0;
-            if (orientacion_inicial <0 ) orientacion_inicial = 3;
-            
-        }
-        
-        return string.Concat(posicion_X,":", posicion_Y,":",orientacion[orientacion_inicial]);
-        
     }
 }
