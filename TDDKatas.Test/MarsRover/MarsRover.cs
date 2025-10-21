692cd5a5008287edbf717730536f7ed58e44dc69
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-20 19:14:14 -0500

    🟢Si_Ingresa_un_comando_distinto_a_M_L_R_DEBE_retornar_ComandoInvalido

--- TDDKatas.Test/MarsRover/MarsRover.cs
+++ TDDKatas.Test/MarsRover/MarsRover.cs
@@ -27,7 +27,7 @@ public class MarsRover
     }
     public string CalcularPosicion(string comandos)
     {
-        
+        if (comandos.Any(comando => comando != 'R' && comando != 'M' && comando != 'L')) return "COMANDO INVALIDO";
         foreach (var comando in comandos.ToCharArray())
         {
             switch (comando)
