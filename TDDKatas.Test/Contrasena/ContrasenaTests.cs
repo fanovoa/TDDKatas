d666a0bd2017d747d4d896ed221459f9b4170e90
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-27 20:34:02 -0500

    🔵 clase para validacion de contraseña

--- TDDKatas.Test/Contrasena/ContrasenaTests.cs
+++ TDDKatas.Test/Contrasena/ContrasenaTests.cs
@@ -4,42 +4,29 @@ namespace TDDKatas.Contrasena;
 
 public class ContrasenaTests
 {
+    
+
     [Fact]
     public void Si_LaContrasenaContieneMenosDe8Caracteres_DEBE_RetornarFalse()
     {
-        var contrasena = "123";
-
-        var validar = esValida(contrasena);
-
+        var contrasena = new Validador("123");
+        var validar = contrasena.esValida();
         validar.Should().Be(false);
     }
 
     [Fact]
     public void Si_LaContrasenaNoTieneMayusculas_DEBE_RetornarFalse()
     {
-        var contrasena = "abcfgdfgdfs";
-
-        var validar = esValida(contrasena);
-
+        var contrasena = new Validador("abcfgdfgdfs");
+        var validar = contrasena.esValida();
         validar.Should().Be(false);
     }
 
     [Fact]
     public void Si_LaContrasenaNoTieneMinusculas_DEBE_RetornarFalse()
     {
-        var contrasena = "ABCDEFGRTWDS";
-
-        var validar = esValida(contrasena);
-
+        var contrasena = new Validador("ABCDEFGRTWDS");
+        var validar = contrasena.esValida();
         validar.Should().Be(false);
-    }
-
-    private bool esValida(string contrasena)
-    {
-        if (contrasena.Length < 9) return false;
-        if (contrasena.Count(char.IsUpper) ==0) return false;
-        if (contrasena.Count(char.IsLower) ==0) return false;
-        return true;
-
     }
 }
---
+++ TDDKatas.Test/Contrasena/Validador.cs
@@ -0,0 +1,17 @@
+﻿namespace TDDKatas.Contrasena;
+
+public class Validador
+{
+    private string _contrasena;
+
+    public Validador(string contrasena)
+    {
+        _contrasena = contrasena;
+    }
+    public bool esValida()
+    {
+        if (_contrasena.Length < 9) return false;
+        if (_contrasena.Count(char.IsUpper) ==0) return false;
+        return _contrasena.Count(char.IsLower) != 0;
+    }
+}
