f4a1a0254b259b00682919ba96e9c2a1abe6c9d1
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-27 20:26:26 -0500

    🔴Si_LaContrasenaNoTieneMinusculas_DEBE_RetornarFalse

--- TDDKatas.Test/Contrasena/ContrasenaTests.cs
+++ TDDKatas.Test/Contrasena/ContrasenaTests.cs
@@ -24,6 +24,16 @@ public class ContrasenaTests
         validar.Should().Be(false);
     }
 
+    [Fact]
+    public void Si_LaContrasenaNoTieneMinusculas_DEBE_RetornarFalse()
+    {
+        var contrasena = "ABCDEFGRTWDS";
+
+        var validar = esValida(contrasena);
+
+        validar.Should().Be(false);
+    }
+
     private bool esValida(string contrasena)
     {
         if (contrasena.Length < 9) return false;
