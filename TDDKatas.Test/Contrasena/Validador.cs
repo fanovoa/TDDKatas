d8732d2406ecfbb858271deefd4594f1ce8947a7
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-10-27 20:35:41 -0500

    💚Si_LaContrasenaNoTieneNumeros_DEBE_RetornarFalse

--- TDDKatas.Test/Contrasena/Validador.cs
+++ TDDKatas.Test/Contrasena/Validador.cs
@@ -12,6 +12,7 @@ public class Validador
     {
         if (_contrasena.Length < 9) return false;
         if (_contrasena.Count(char.IsUpper) ==0) return false;
+        if (_contrasena.Count(char.IsDigit) == 0) return false;
         return _contrasena.Count(char.IsLower) != 0;
     }
 }
