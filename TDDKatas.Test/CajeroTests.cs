2f05c6535548621a9edc2330adad89e730fff17d
Victor Fabian Novoa <victor.novoa@sinco.com.co>
2025-11-03 20:44:32 -0500

    🔴SiRetira1_Debe_Regresar1de1

---
+++ TDDKatas.Test/CajeroTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+
+namespace TDDKatas;
+
+public class CajeroTests
+{
+    [Fact]
+    public void SiRetira1_Debe_Regresar1de1()
+    {
+        var cajero = new Cajero();
+        var denominaciones = cajero.Retirar(1);
+        denominaciones.Should().BeEquivalentTo((new List<(int, int)>{(1,1)}));
+    }
+}
+
+public class Cajero
+{
+    public object Retirar(int i)
+    {
+        throw new NotImplementedException();
+    }
+}
